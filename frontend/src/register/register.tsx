f3095179ca9c64110ba338f02db8804f4d5a017c
quentbt <quentinbuet4@gmail.com>
2025-07-23 16:45:20 +0200

    <Fix>(login): modification des boutons en fonction du choix d'inscription

--- frontend/src/register/register.tsx
+++ frontend/src/register/register.tsx
@@ -317,14 +317,28 @@ function Register() {
                                         >
                                             Retour
                                         </button>
-                                        <button
-                                            type="button"
-                                            onClick={() => setStep(3)}
-                                            disabled={!userType}
-                                            className="py-3 px-6 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold rounded-xl hover:from-purple-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all"
-                                        >
-                                            Suivant
-                                        </button>
+
+                                        {userType === 'user' ? (
+                                            <button
+                                                type="submit"
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    handleSubmit();
+                                                }}
+                                                className="py-3 px-6 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold rounded-xl hover:from-purple-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all"
+                                            >
+                                                Créer un compte
+                                            </button>
+                                        ) : (
+                                            <button
+                                                type="button"
+                                                onClick={() => setStep(3)}
+                                                disabled={!userType}
+                                                className="py-3 px-6 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold rounded-xl hover:from-purple-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all"
+                                            >
+                                                Suivant
+                                            </button>
+                                        )}
                                     </div>
                                 </>
                             )}
